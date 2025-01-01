e1b6e0070a3a0fed7ebc46ae3e405abe2e9906f2
Mrunal fulzele <mrn26893@gmail.com>
2025-01-01 13:12:20 +0530

    One Boook Page lazy loading

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -46,15 +46,19 @@ class BookController extends Controller
      */
     public function store(Request $request)
     {
-        //
+
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Book $book)
     {
-        //
+        return view('books.show',
+        [
+            'book' => $book->load([
+                'reviews' => fn($query) => $query->latest()
+                ])]);
     }
 
     /**
